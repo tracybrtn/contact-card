cf0f9267d9ed7d882e404d8bdf17e159ce99b4c7
tracybrtn <tracynburton@gmail.com>
2022-11-14 16:20:22 -0600

    images loading

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,2 +1,15 @@
+// Import modules
 import './form';
 import './submit';
+
+// Import images
+import Logo from '../images/logo.png';
+import Bear from '../images/bear.png';
+import Dog from '../images/dog.png';
+
+// Add images on load
+window.addEventListener('load', function () {
+  document.getElementById('logo').src = Logo;
+  document.getElementById('bearThumbnail').src = Bear;
+  document.getElementById('dogThumbnail').src = Dog;
+});
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,7 +3,15 @@ const path = require('path');
 module.exports = {
   entry: './src/js/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ]
+  }
 };
