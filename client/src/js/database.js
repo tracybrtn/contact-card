d0cdefe9f9e4b8c4550e4fd294672f4604640bc7
tracybrtn <tracynburton@gmail.com>
2022-11-14 18:20:46 -0600

    edit functionality added

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -79,3 +79,21 @@ export const deleteDb = async(id) => {
   console.log('result.value', result);
   return result?.value;
 }
+
+export const editDb = async(id, name, email, phone, profile) => {
+  console.log('PUT to the database', id);
+
+  // Create a connection to the IndexedDB database and the version we want to use.
+  const contactDb = await openDB('contact_db', 1);
+
+  // Create a new transaction and specify the store and data privileges.
+  const tx = contactDb.transaction('contacts', 'readwrite');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('contacts');
+
+  // Use the .put() method on the store and pass in the content.
+  const request = store.put({ id: id, name: name, email: email, phone: phone, profile: profile });
+  const result = await request;
+  console.log('🚀 - data saved to the database', result);
+}
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -1,6 +1,6 @@
 // Import modules
 import { clearForm, toggleForm } from './form';
-import { initDb, getDb, postDb, deleteDb } from './database';
+import { initDb, getDb, postDb, deleteDb, editDb } from './database';
 import { fetchCards } from './cards';
 
 // Import CSS files
@@ -47,6 +47,15 @@ form.addEventListener('submit', event => {
     postDb(name, email, phone, profile);
   } else {
   
+    // Obtains values passed into the form element
+    let name = document.getElementById("name").value;
+    let phone = document.getElementById("phone").value;
+    let email = document.getElementById("email").value;
+    let profile = document.querySelector('input[type="radio"]:checked').value;
+
+    // Calls the editDB function passing in any values from the form element as well as the ID of the contact that we are updating
+    editDb(profileId, name, email, phone, profile);
+
     fetchCards();
       // Toggles the submit button back to POST functionality
     submitBtnToUpdate = false;
@@ -68,4 +77,22 @@ window.deleteCard = (e) => {
   deleteDb(id);
   // Reload the DOM
   fetchCards();
+};
+
+window.editCard = (e) => {
+  // Grabs the id from the button element attached to the contact card and sets a global variable that will be used in the form element.
+  profileId = parseInt(e.dataset.id);
+  
+  // Grabs information to pre-populate edit form
+  let editName = e.dataset.name;
+  let editEmail = e.dataset.email;
+  let editPhone = e.dataset.phone;
+  
+  document.getElementById("name").value = editName;
+  document.getElementById("email").value = editEmail;
+  document.getElementById("phone").value = editPhone;
+
+  form.style.display = "block";
+  // Toggles the submit button so that it now Updates an existing contact instead of posting a new one
+  submitBtnToUpdate = true;
 };
